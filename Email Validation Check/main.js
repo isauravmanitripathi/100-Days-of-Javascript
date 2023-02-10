74e709403b42c326a51014d06990855043ccd3a4
spmtripathi10@gmail.com <SIDqwer123@>
2023-02-11 00:24:34 +0530

    completed day 1

---
+++ Email Validation Check/main.js
@@ -0,0 +1,28 @@
+// validate email
+const validateEmail = () => {
+    const msg = document.querySelector(".field-input span");
+    const icon = document.querySelector(".field-input i");
+    const emailValue = document.querySelector("#email").value;
+    const regexEmail = /^w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+    if (emailValue.match(regexEmail)) {
+        ms.classList.remove("invalid");
+        msg.classList.add("valid");
+        msg.innerHTML = "Email Valid";
+        icon.classList.remove("fa-exclamation-circle");
+        icon.classList.add("fa-check-circle");
+    } else {
+        msg.classList.add("invalid");
+        msg.classList.remove("valid");
+        msg.innerHTML.add("fa-exclamation-circle");
+        icon.classList.remove("fa-check-circle");
+    }
+
+    if (emailValue === "") {
+        msg.innerHTML ="";
+        msg.style.opacity = 0;
+        icon.classList.remove("fa-exclamation-circle");
+    } else {
+        msg.style.opacity = 1;
+    }
+}
